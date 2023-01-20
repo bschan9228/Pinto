ff7a1fd77f4dc831894dcfc244ed6d4d996dfe38
Byron Chan <bschan@UCSD.EDU>
2023-01-20 03:17:13 -0800

    Updated pcb

--- Software/Experimental/ESP32-S3-LCD/src/main.cpp
+++ Software/Experimental/ESP32-S3-LCD/src/main.cpp
@@ -1,193 +1,156 @@
-#include <Arduino.h>
 /*
- Font draw speed and flicker test, draws all numbers 0-999 in each font
- Average time in milliseconds to draw is shown in red
- A total of 2890 characters are drawn in each font
+ An example analogue clock using a TFT LCD screen to show the time
+ use of some of the drawing commands with the library.
+ For a more accurate clock, it would be better to use the RTClib library.
+ But this is just a demo.
 
- Needs fonts 2, 4, 6, 7 and 8
+ This sketch uses font 4 only.
  Make sure all the display driver and pin connections are correct by
  editing the User_Setup.h file in the TFT_eSPI library folder.
  #########################################################################
  ###### DON'T FORGET TO UPDATE THE User_Setup.h FILE IN THE LIBRARY ######
  #########################################################################
+
+ Based on a sketch by Gilchrist 6/2/2014 1.0
  */
 
-#include <TFT_eSPI.h> // Hardware-specific library
 #include <SPI.h>
+#include <TFT_eSPI.h> // Hardware-specific library
+
+#define TFT_GREY 0x5AEB
 
 TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 
-unsigned long drawTime = 0;
+float sx = 0, sy = 1, mx = 1, my = 0, hx = -1, hy = 0; // Saved H, M, S x & y multipliers
+float sdeg = 0, mdeg = 0, hdeg = 0;
+uint16_t osx = 120, osy = 120, omx = 120, omy = 120, ohx = 120, ohy = 120; // Saved H, M, S x & y coords
+uint16_t x0 = 0, x1 = 0, yy0 = 0, yy1 = 0;
+uint32_t targetTime = 0; // for next 1 second timeout
+
+static uint8_t conv2d(const char *p);                                                // Forward declaration needed for IDE 1.6.x
+uint8_t hh = conv2d(__TIME__), mm = conv2d(__TIME__ + 3), ss = conv2d(__TIME__ + 6); // Get H, M, S from compile time
+
+bool initial = 1;
 
 void setup(void)
 {
   tft.init();
-  tft.setRotation(1);
+  tft.setRotation(0);
+
+  // tft.fillScreen(TFT_BLACK);
+  // tft.fillScreen(TFT_RED);
+  // tft.fillScreen(TFT_GREEN);
+  // tft.fillScreen(TFT_BLUE);
+  // tft.fillScreen(TFT_BLACK);
+  tft.fillScreen(TFT_GREY);
+
+  tft.setTextColor(TFT_WHITE, TFT_GREY); // Adding a background colour erases previous text automatically
+
+  // Draw clock face
+  tft.fillCircle(120, 120, 118, TFT_GREEN);
+  tft.fillCircle(120, 120, 110, TFT_BLACK);
+
+  // Draw 12 lines
+  for (int i = 0; i < 360; i += 30)
+  {
+    sx = cos((i - 90) * 0.0174532925);
+    sy = sin((i - 90) * 0.0174532925);
+    x0 = sx * 114 + 120;
+    yy0 = sy * 114 + 120;
+    x1 = sx * 100 + 120;
+    yy1 = sy * 100 + 120;
+
+    tft.drawLine(x0, yy0, x1, yy1, TFT_GREEN);
+  }
+
+  // Draw 60 dots
+  for (int i = 0; i < 360; i += 6)
+  {
+    sx = cos((i - 90) * 0.0174532925);
+    sy = sin((i - 90) * 0.0174532925);
+    x0 = sx * 102 + 120;
+    yy0 = sy * 102 + 120;
+    // Draw minute markers
+    tft.drawPixel(x0, yy0, TFT_WHITE);
+
+    // Draw main quadrant dots
+    if (i == 0 || i == 180)
+      tft.fillCircle(x0, yy0, 2, TFT_WHITE);
+    if (i == 90 || i == 270)
+      tft.fillCircle(x0, yy0, 2, TFT_WHITE);
+  }
+
+  tft.fillCircle(120, 121, 3, TFT_WHITE);
+
+  // Draw text at position 120,260 using fonts 4
+  // Only font numbers 2,4,6,7 are valid. Font 6 only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : . - a p m
+  // Font 7 is a 7 segment font and only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : .
+  tft.drawCentreString("Time flies", 120, 260, 4);
+
+  targetTime = millis() + 1000;
 }
 
 void loop()
 {
-
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-
-  drawTime = millis();
-
-  for (int i = 0; i < 1000; i++)
+  if (targetTime < millis())
   {
-    yield();
-    tft.drawNumber(i, 100, 80, 1);
+    targetTime += 1000;
+    ss++; // Advance second
+    if (ss == 60)
+    {
+      ss = 0;
+      mm++; // Advance minute
+      if (mm > 59)
+      {
+        mm = 0;
+        hh++; // Advance hour
+        if (hh > 23)
+        {
+          hh = 0;
+        }
+      }
+    }
+
+    // Pre-compute hand degrees, x & y coords for a fast screen update
+    sdeg = ss * 6;                     // 0-59 -> 0-354
+    mdeg = mm * 6 + sdeg * 0.01666667; // 0-59 -> 0-360 - includes seconds
+    hdeg = hh * 30 + mdeg * 0.0833333; // 0-11 -> 0-360 - includes minutes and seconds
+    hx = cos((hdeg - 90) * 0.0174532925);
+    hy = sin((hdeg - 90) * 0.0174532925);
+    mx = cos((mdeg - 90) * 0.0174532925);
+    my = sin((mdeg - 90) * 0.0174532925);
+    sx = cos((sdeg - 90) * 0.0174532925);
+    sy = sin((sdeg - 90) * 0.0174532925);
+
+    if (ss == 0 || initial)
+    {
+      initial = 0;
+      // Erase hour and minute hand positions every minute
+      tft.drawLine(ohx, ohy, 120, 121, TFT_BLACK);
+      ohx = hx * 62 + 121;
+      ohy = hy * 62 + 121;
+      tft.drawLine(omx, omy, 120, 121, TFT_BLACK);
+      omx = mx * 84 + 120;
+      omy = my * 84 + 121;
+    }
+
+    // Redraw new hand positions, hour and minute hands not erased here to avoid flicker
+    tft.drawLine(osx, osy, 120, 121, TFT_BLACK);
+    osx = sx * 90 + 121;
+    osy = sy * 90 + 121;
+    tft.drawLine(osx, osy, 120, 121, TFT_RED);
+    tft.drawLine(ohx, ohy, 120, 121, TFT_WHITE);
+    tft.drawLine(omx, omy, 120, 121, TFT_WHITE);
+    tft.drawLine(osx, osy, 120, 121, TFT_RED);
+
+    tft.fillCircle(120, 121, 3, TFT_RED);
   }
-
-  drawTime = millis() - drawTime;
-
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  int xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 100)
-    tft.drawString("Font 1 not loaded!", 50, 210, 4);
-
-  delay(4000);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  drawTime = millis();
-
-  for (int i = 0; i < 1000; i++)
-  {
-    yield();
-    tft.drawNumber(i, 100, 80, 2);
-  }
-
-  drawTime = millis() - drawTime;
-
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 200)
-    tft.drawString("Font 2 not loaded!", 50, 210, 4);
-
-  delay(4000);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  drawTime = millis();
-
-  for (int i = 0; i < 1000; i++)
-  {
-    yield();
-    tft.drawNumber(i, 100, 80, 4);
-  }
-
-  drawTime = millis() - drawTime;
-
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 200)
-    tft.drawString("Font 4 not loaded!", 50, 210, 4);
-
-  delay(4000);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  drawTime = millis();
-
-  for (int i = 0; i < 1000; i++)
-  {
-    yield();
-    tft.drawNumber(i, 100, 80, 6);
-  }
-
-  drawTime = millis() - drawTime;
-
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 200)
-    tft.drawString("Font 6 not loaded!", 50, 210, 4);
-
-  delay(4000);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  drawTime = millis();
-
-  for (int i = 0; i < 1000; i++)
-  {
-    yield();
-    tft.drawNumber(i, 100, 80, 7);
-  }
-
-  drawTime = millis() - drawTime;
-
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 200)
-    tft.drawString("Font 7 not loaded!", 50, 210, 4);
-
-  delay(4000);
-  tft.fillScreen(TFT_YELLOW);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  drawTime = millis();
-
-  for (int i = 0; i < 1000; i++)
-  {
-    yield();
-    tft.drawNumber(i, 100, 80, 8);
-  }
-
-  drawTime = millis() - drawTime;
-
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 200)
-    tft.drawString("Font 8 not loaded!", 50, 210, 4);
-
-  delay(4000);
 }
 
-// #include <stdint.h>
-// #include "TouchScreen.h"
-// #include <Arduino.h>
-// #include <USB.h>
-// Serial.begin(115200);
-
-// #define YP 21 // must be an analog pin, use "An" notation!
-// #define XM 26 // must be an analog pin, use "An" notation!
-// #define YM 47 // can be a digital pin
-// #define XP 33 // can be a digital pin
-
-// // For better pressure precision, we need to know the resistance
-// // between X+ and X- Use any multimeter to read it
-// // For the one we're using, its 300 ohms across the X plate
-// TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
-
-// void setup(void)
-// {
-//   USBSerial.begin(9600);
-// }
-
-// void loop(void)
-// {
-//   // a point object holds x y and z coordinates
-//   TSPoint p = ts.getPoint();
-
-//   // we have some minimum pressure we consider 'valid'
-//   // pressure of 0 means no pressing!
-//   if (p.z > ts.pressureThreshhold)
-//   {
-//     USBSerial.print("X = ");
-//     USBSerial.print(p.x);
-//     USBSerial.print("\tY = ");
-//     USBSerial.print(p.y);
-//     USBSerial.print("\tPressure = ");
-//     USBSerial.println(p.z);
-//   }
-
-//   delay(100);
-// }
+static uint8_t conv2d(const char *p)
+{
+  uint8_t v = 0;
+  if ('0' <= *p && *p <= '9')
+    v = *p - '0';
+  return 10 * v + *++p - '0';
+}
